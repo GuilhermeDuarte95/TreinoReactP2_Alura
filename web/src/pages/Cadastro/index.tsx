698fc4aeb4f60018d481697d7ddf24629b2e6a43
Guilherme Duarte <guilhermeduarte.informatica@gmail.com>
2024-10-29 20:02:26 -0300

    [FEAT] Criação do formulário login e cadastro (obs cadastro falta terminar)

---
+++ web/src/pages/Cadastro/index.tsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import logo from "./logo.png"
+import { Step, StepLabel, Stepper } from "@mui/material";
+import { useState } from "react";
+
+const Imagem = styled.img`
+  padding: 2em 0;
+`;
+
+interface PropsCustomizadas {
+    cor: string
+}
+
+const StepCustomizado = styled.div<PropsCustomizadas>`
+    background-color: ${({cor}) => cor};
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+`
+
+export default function Cadastro(){
+    const [etapaAtiva, setEtapaAtiva ] = useState(0);
+    return (
+        <>
+            <Imagem src={logo} alt="Logo da Voll"/>
+            <Stepper activeStep={etapaAtiva}>
+                <Step>
+                    <StepLabel
+                        StepIconComponent={(props) =>(
+                            <StepCustomizado cor={props.active ? 'lightblue' : 'lightgray'}/>
+                        )}
+                    />
+                </Step>
+                <Step>
+                <StepLabel
+                        StepIconComponent={(props) =>(
+                            <StepCustomizado cor={props.active ? 'lightblue' : 'lightgray'}/>
+                        )}
+                    />
+                </Step>
+            </Stepper>
+        </>
+    )
+}
