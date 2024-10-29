b4a2f471eeafde92f4e13f278b8e3ca09c2994bb
Guilherme Duarte <guilhermeduarte.informatica@gmail.com>
2024-10-29 17:19:57 -0300

    [FEAT] Criado AppRota + pasta Page com todas as páginas dentro + componente input

---
+++ web/src/pages/PaginaInicial/Banner/index.tsx
@@ -0,0 +1,16 @@
+//Arquivo index.tsx dentro da pasta Banner
+
+import styled from "styled-components";
+import banner from './banner.png'
+
+
+const Banner = styled.section`
+height: 500px;
+width: 100%;
+background-image: url(${banner});
+background-repeat: no-repeat;
+background-size: cover;
+`
+
+
+export default Banner;
